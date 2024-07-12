8c962400a6e61ebd72bb6f32ab24b9cefde83ed5
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-07-12 16:25:54 +0200

    feat: add ppr (pre-partial rendering) functionality to layout page

--- nextjs-minimal-dashboard/next.config.mjs
+++ nextjs-minimal-dashboard/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        ppr: "incremental", // Allows to adopt ppr for specific routes 
+    }
+};
 
 export default nextConfig;
